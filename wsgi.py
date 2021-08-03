63da576d7eb3b57b4667db02a48dcee07418a940
Matt Shirley <mdshw5@gmail.com>
2021-08-03 14:25:21 -0400

    Change module paths.

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
-from app import app
-from views import *
-from models import *
+from microblog.app import app
+from microblog.views import *
+from microblog.models import *
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
