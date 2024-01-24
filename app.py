229c56b86008597507ec01d15cffef79290fc11d
Matt Shirley <mdshw5@gmail.com>
2024-01-24 15:26:23 +0000

    Remove session config

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
 from flask import Flask
-from flask_caching import Cache
-import os
-
-SECRET_KEY = os.urandom(24)
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
